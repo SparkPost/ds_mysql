a779ece83707d66b87f58ba66279de8c0efa106d
Douglas Koerich <douglas.koerich@messagebird.com>
2024-01-08 16:14:39 -0300

    Revert "ESC-2481: more fixes in test instructions"
    This reverts commit c3fa07beb7b135af28f5bf9ac39c8af93843e6cc.

--- create_ectest.sql
+++ create_ectest.sql
@@ -1,6 +1,5 @@
-CREATE DATABASE ectest;
-CREATE USER ectest IDENTIFIED BY 'ectest';
-GRANT ALL ON ectest.* TO ectest;
-USE ectest;
+create database ectest;
+grant all on ectest.* to ectest identified by 'ectest';
+use ectest;
 CREATE TABLE `accounts` (`name` varchar(255) NOT NULL default '' PRIMARY KEY);
 INSERT INTO `accounts` VALUES ('good'),('good1'),('good2'),('good3'),('good4');
