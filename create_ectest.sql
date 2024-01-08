c3fa07beb7b135af28f5bf9ac39c8af93843e6cc
Douglas Koerich <douglas.koerich@messagebird.com>
2024-01-08 15:31:45 -0300

    ESC-2481: more fixes in test instructions
    Signed-off-by: Douglas Koerich <douglas.koerich@messagebird.com>

--- create_ectest.sql
+++ create_ectest.sql
@@ -1,5 +1,6 @@
-create database ectest;
-grant all on ectest.* to ectest identified by 'ectest';
-use ectest;
+CREATE DATABASE ectest;
+CREATE USER ectest IDENTIFIED BY 'ectest';
+GRANT ALL ON ectest.* TO ectest;
+USE ectest;
 CREATE TABLE `accounts` (`name` varchar(255) NOT NULL default '' PRIMARY KEY);
 INSERT INTO `accounts` VALUES ('good'),('good1'),('good2'),('good3'),('good4');
